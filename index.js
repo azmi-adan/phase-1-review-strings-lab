e294ace4cdf90dfd1132da1a4eeb6cafd8ddc198
azmi adan <azmi.adan2032@gmail.com>
2024-06-25 07:59:07 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+// Define welcome message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define excited welcome message
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define short greeting
+const firstInitial = currentUser[0]; // Get the first character (initial) of the name
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
+// Export variables (if needed)
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting
+};
+
+
